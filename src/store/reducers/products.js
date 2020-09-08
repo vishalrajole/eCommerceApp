53bcf645465b2810ffc23d1a8523bf5f5fa7fafe
VishalRajole <vishal.rajole@mobikontech.com>
2020-09-08 07:24:36 +0200

    fixed delete product functionality

--- src/store/reducers/products.js
+++ src/store/reducers/products.js
@@ -1,10 +1,24 @@
 import PRODUCTS from "../../__mocks__/data";
+import { DELETE_PRODUCT } from "../actions/products";
 
 const initialState = {
   availableProducts: PRODUCTS,
   userProducts: PRODUCTS.filter((product) => product.ownerId === "u1"),
 };
 
-export default (state = initialState) => {
-  return state;
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case DELETE_PRODUCT:
+      return {
+        ...state,
+        userProducts: state.userProducts.filter((product) => {
+          return product.id !== action.productId;
+        }),
+        availableProducts: state.availableProducts.filter((product) => {
+          return product.id !== action.productId;
+        }),
+      };
+    default:
+      return state;
+  }
 };
