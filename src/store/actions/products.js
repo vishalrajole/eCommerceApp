c4c0cf94a830dd2f8e81124508d5b330b643c0ee
vishalrajole <vishalrajole1991@gmail.com>
2020-09-10 09:11:08 +0200

    edit and add products

--- src/store/actions/products.js
+++ src/store/actions/products.js
@@ -1,5 +1,31 @@
 export const DELETE_PRODUCT = "DELETE_PRODUCT";
+export const CREATE_PRODUCT = "CREATE_PRODUCT";
+export const UPDATE_PRODUCT = "UPDATE_PRODUCT";
 
 export const deleteProduct = (productId) => {
   return { type: DELETE_PRODUCT, productId: productId };
+};
+
+export const createProduct = ({ title, description, imageUrl, price }) => {
+  return {
+    type: CREATE_PRODUCT,
+    product: {
+      title,
+      description,
+      imageUrl,
+      price,
+    },
+  };
+};
+
+export const updateProduct = ({ productId, title, description, imageUrl }) => {
+  return {
+    type: UPDATE_PRODUCT,
+    productId: productId,
+    product: {
+      title,
+      description,
+      imageUrl,
+    },
+  };
 };
