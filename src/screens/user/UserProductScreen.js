97e42166e3d696b15c32457669faeba321220c8a
vishalrajole <vishalrajole1991@gmail.com>
2020-09-11 08:02:08 +0200

    fixed navigation after product edit and delete

--- src/screens/user/UserProductScreen.js
+++ src/screens/user/UserProductScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, FlatList, Button } from "react-native";
+import { StyleSheet, FlatList, Button, Alert } from "react-native";
 import { useSelector, useDispatch } from "react-redux";
 import ProductItem from "../../components/ProductItem";
 import { deleteProduct } from "../../store/actions/products";
@@ -8,6 +8,19 @@ import Colors from "../../styles/colors";
 const UserProductScreen = (props) => {
   const userProducts = useSelector((state) => state.products.userProducts);
   const dispatch = useDispatch();
+
+  const deleteHandler = (productId) => {
+    Alert.alert("Delete product", "Do you want to delete this product?", [
+      { text: "No", style: "default" },
+      {
+        text: "Yes",
+        style: "destructive",
+        onPress: () => {
+          dispatch(deleteProduct(productId));
+        },
+      },
+    ]);
+  };
 
   return (
     <FlatList
@@ -33,7 +46,7 @@ const UserProductScreen = (props) => {
             color={Colors.primary}
             title="Delete"
             onPress={() => {
-              dispatch(deleteProduct(itemData.item.id));
+              deleteHandler(itemData.item.id);
             }}
           ></Button>
         </ProductItem>
